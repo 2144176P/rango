cc2ad942a17cec3e1eb1fbec474d629a76187d3b
Charlie Parker <2144176p@student.gla.ac.uk>
2017-02-08 20:26:31 +0000

    updated index view to use template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!  <a href='/rango/about/'>About</a>")
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # NB The key 'boldmessage' is the same as {{ boldmessage }} in the HTML template
+    context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to client.
+    # We make use of the shortcut function
+    # NB the first parameter is the template we wish to use i.e rango/index.html
+    return render(request, 'rango/index.html', context=context_dict)
+
+
 
 def about(request):
     return HttpResponse("Rango says here is the about page.  <a href='/rango/'>Index</a>")
