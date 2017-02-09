628b2d3a6c509d70df100d07bb47c87adc7b7f12
Charlie Parker <2144176p@student.gla.ac.uk>
2017-02-09 18:58:04 +0000

    updated views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,7 +5,7 @@ from rango.models import Category
 
 def index(request):
 
-    category_list = Category.objects.orger_by('-likes')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
 
     # NB the first parameter is the template we wish to use i.e rango/index.html
