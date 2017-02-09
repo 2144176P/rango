23f945fef2a5cc1fc532c897b24cf4cde9660349
Charlie Parker <2144176p@student.gla.ac.uk>
2017-02-09 18:11:58 +0000

    Updated Index view and added image to about page template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from rango.models import Category
 
 def index(request):
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # NB The key 'boldmessage' is the same as {{ boldmessage }} in the HTML template
-    context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.orger_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Return a rendered response to send to client.
-    # We make use of the shortcut function
     # NB the first parameter is the template we wish to use i.e rango/index.html
     return render(request, 'rango/index.html', context=context_dict)
 
