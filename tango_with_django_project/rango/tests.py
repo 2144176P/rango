1dac40b7a6b5b5d0d14d82c97283ac60165cb878
Charlie Parker <2144176p@student.gla.ac.uk>
2017-02-10 16:13:06 +0000

    refactored all templates

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -5,4 +5,10 @@ class CategoryMethodTests(TestCase):
     def test_ensure_views_are_positive(self):
         cat = Category(name='test', views=-1, likes=0)
         cat.save()
-        selt.assertEqual((cat.views >= 0), True)
+        self.assertEqual((cat.views >= 0), True)
+
+    def test_slug_line_creation(self):
+        cat = cat('Random Category String')
+        cat.save()
+        self.assertEqual(cat.slug, 'random-category-string')
+
